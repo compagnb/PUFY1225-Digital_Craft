9c142139ab8139bd17fd36bc629defe7d335a48d
Barbara Compagnoni <compagnb@newschool.edu>
2020-02-18 19:49:07 -0500

    P5 to SVG Template!

---
+++ p5svgTemplate/js/sketch.js
@@ -0,0 +1,21 @@
+
+
+function setup() {
+    createCanvas(600, 400, SVG); // Create SVG Canvas
+    strokeWeight(2);
+    stroke('#ED225D');
+}
+
+function draw() {
+
+	for(var x=0; x < width; x+=xRad){
+    	for(var y=0; y < height; y+=yRad){
+    		line(x, y, 200, 200);
+    	}
+	}
+	print(frameCount);
+  	// if (frameCount > 100) {
+        noLoop();
+        save();
+    // }
+}
